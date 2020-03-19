09954fcc5cf89985ef7237eb3650948b68d1c45c
ThibaudMassin <62330292+ThibaudMassin@users.noreply.github.com>
2020-03-19 10:57:10 +0100

    Fin de séance + ajout ex1

--- td1/ex1.php
+++ td1/ex1.php
@@ -1,5 +1,5 @@
 <a href="?message=Hello">Afficher Hello</a>
-<a href="?message=Message">Afficher Hello</a>
-<a href="?message=Hello">Afficher Hello</a>
+<a href="?message=Bonjour">Afficher Hello</a>
+<a href="?message=Hola">Afficher Hello</a>
 <?php
 echo "Vous êtes sur la page n° " . $_GET["page"];
