3fe86463c7abaae73e73df83f80e72c60559c651
ThibaudMassin <62330292+ThibaudMassin@users.noreply.github.com>
2020-03-25 09:58:41 +0100

    TP1

--- td1/ex1.php
+++ td1/ex1.php
@@ -1,5 +1,10 @@
-<a href="?message=Hello">Afficher Hello</a>
-<a href="?message=Bonjour">Afficher Hello</a>
-<a href="?message=Hola">Afficher Hello</a>
+
+$_GET=<a href="?message=Hello">Afficher Hello</a>
+
+$_GET=<a href="?message=Bonjour">Afficher Hello</a>
+
+$_GET=<a href="?message=Hola">Afficher Hello</a>
+
 <?php
+
 echo "Vous êtes sur la page n° " . $_GET["page"];
